45444b716aea2274defef213bdf25d751a82df0c
daniel-hau-shan12 <127985769+daniel-hau-shan12@users.noreply.github.com>
2024-01-15 13:22:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Create the app
+# Create the game
+# Build the game
+# Run the game
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+  pass
+
+class PongApp(App):
+  def build(self):
+    return PongGame()
+
+
+PongApp().run()
